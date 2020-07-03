e934d4a364068a32b25b7461865d4a2c25bef0be
yxb <soldieryu.dev@gmail.com>
2020-07-03 18:42:49 +0800

    [Func]: BST

--- src/playwithalgorithm/05/BST.java
+++ src/playwithalgorithm/05/BST.java
@@ -1,7 +1,9 @@
+import java.util.LinkedList;
+
 /**
  * Author   : soldieryu.dev@gmail.com
  * Create   : 2020/6/18
- * Describe :
+ * Describe : 二分搜索树
  */
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;  // 根节点
@@ -16,6 +18,13 @@ public class BST<Key extends Comparable<Key>, Value> {
             this.key = key;
             this.value = value;
             left = right = null;
+        }
+
+        public Node(Node node) {
+            this.key = node.key;
+            this.value = node.value;
+            this.left = node.left;
+            this.right = node.right;
         }
 
     }
@@ -51,7 +60,6 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
 
-
     // 向以node为根的二分搜索树中, 插入节点(key, value), 使用递归算法
     // 返回插入新节点后的二分搜索树的根
     public Node insert(Node node, Key key, Value value) {
@@ -81,7 +89,7 @@ public class BST<Key extends Comparable<Key>, Value> {
             return true;
         } else if (key.compareTo(node.key) < 0) {
             return contain(node.left, key);
-        }else{
+        } else {
             return contain(node.right, key);
         }
     }
@@ -98,12 +106,173 @@ public class BST<Key extends Comparable<Key>, Value> {
             return node.value;
         } else if (key.compareTo(node.key) < 0) {
             return search(node.left, key);
-        }else{
+        } else {
             return search(node.right, key);
         }
     }
 
+    // 对以node为根的二叉搜索树进行前序遍历, 递归算法
+    private void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.value);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
 
+    // 对以node为根的二叉搜索树进行中序遍历, 递归算法
+    private void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.println(node.value);
+            inOrder(node.right);
+        }
+    }
+
+    // 对以node为根的二叉搜索树进行后序遍历, 递归算法
+    private void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.value);
+        }
+    }
+
+    //二分搜索树的层序遍历
+    public void levelOrder() {
+        //使用LinkedList来作为我们的队列
+        LinkedList<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node node = q.remove();
+            System.out.println(node.key);
+            if (node.left != null) {
+                q.add(node.left);
+            } else if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+    }
+
+    // 寻找二分搜索树的最小的键值
+    public Key minimum() {
+        assert count != 0;
+        Node minNode = minimum(root);
+        return minNode.key;
+
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+
+    }
+
+    // 寻找二分搜索树的最大的键值
+    public Key maximum() {
+        assert count != 0;
+        Node maxNode = maximum(root);
+        return maxNode.key;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node.right;
+        }
+        return maximum(node.right);
+    }
+
+    // 从二分搜索树中删除最小值所在节点
+    public void removeMin() {
+        if (root != null)
+            root = removeMin(root);
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            count--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+
+    }
+
+    // 从二分搜索树中删除最大值所在节点
+    public void removeMax() {
+        if (root != null)
+            root = removeMax(root);
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            count--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    // 从二分搜索树中删除键值为key的节点
+    private void remove(Key key) {
+        root = remove(root, key);
+    }
+
+    // 删除掉以node为根的二分搜索树中键值为key的节点, 递归算法
+    // 返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+
+        //待删除的节点键值小于当前节点的键值
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+
+            //待删除的节点键值大于当前节点的键值
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {
+            //待删除的节点键值等于当前节点的键值
+
+            //如果没有右子节点
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                count--;
+                return leftNode;
+            }
+            if (node.left == null) {
+                //如果没有左子节点
+                Node rightNode = node.right;
+                node.right = null;
+                count--;
+                return rightNode;
+            }
+            Node successor = new Node(minimum(node.right));
+            count++;
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            count--;
+
+            return successor;
+        }
+    }
 
 
     // 测试二分搜索树
@@ -113,12 +282,12 @@ public class BST<Key extends Comparable<Key>, Value> {
 
         // 创建一个数组，包含[0...N)的所有元素
         Integer[] arr = new Integer[N];
-        for(int i = 0 ; i < N ; i ++)
+        for (int i = 0; i < N; i++)
             arr[i] = new Integer(i);
 
         // 打乱数组顺序
-        for(int i = 0 ; i < N ; i ++){
-            int pos = (int) (Math.random() * (i+1));
+        for (int i = 0; i < N; i++) {
+            int pos = (int) (Math.random() * (i + 1));
             Integer t = arr[pos];
             arr[pos] = arr[i];
             arr[i] = t;
@@ -132,16 +301,16 @@ public class BST<Key extends Comparable<Key>, Value> {
 
         // 我们测试用的的二分搜索树的键类型为Integer，值类型为String
         // 键值的对应关系为每个整型对应代表这个整型的字符串
-        BST<Integer,String> bst = new BST<Integer,String>();
-        for(int i = 0 ; i < N ; i ++)
+        BST<Integer, String> bst = new BST<Integer, String>();
+        for (int i = 0; i < N; i++)
             bst.insert(new Integer(arr[i]), Integer.toString(arr[i]));
 
         // 对[0...2*N)的所有整型测试在二分搜索树中查找
         // 若i在[0...N)之间，则能查找到整型所对应的字符串
         // 若i在[N...2*N)之间，则结果为null
-        for(int i = 0 ; i < 2*N ; i ++){
+        for (int i = 0; i < 2 * N; i++) {
             String res = bst.search(new Integer(i));
-            if( i < N )
+            if (i < N)
                 assert res.equals(Integer.toString(i));
             else
                 assert res == null;
@@ -155,29 +324,5 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     }
 
-    // 对以node为根的二叉搜索树进行前序遍历, 递归算法
-    private void preOrder(Node node){
-        if (node != null) {
-            System.out.println(node.value);
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-    }
-    // 对以node为根的二叉搜索树进行中序遍历, 递归算法
-    private void inOrder(Node node){
-        if (node != null) {
-            inOrder(node.left);
-            System.out.println(node.value);
-            inOrder(node.right);
-        }
-    }
-    // 对以node为根的二叉搜索树进行后序遍历, 递归算法
-    private void postOrder(Node node){
-        if (node != null) {
-            postOrder(node.left);
-            postOrder(node.right);
-            System.out.println(node.value);
-        }
-    }
 
 }
